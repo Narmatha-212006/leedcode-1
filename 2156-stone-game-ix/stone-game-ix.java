3a091b94e059f06454f5649a4c56175afd185055
Narmatha M <narma21012006@gmail.com>
2026-08-16 23:05:23 +0530

    Time: 4 ms (75.68%) | Memory: 114.8 MB (64.86%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int arr[]=new int[3];
+        for(int stone:stones){
+            arr[stone%3]+=1;
+        }
+        if(arr[0]%2==0){
+            return arr[1]>0 && arr[2]>0; 
+        }
+        else{
+            return Math.abs(arr[1]-arr[2])>2;
+        }
+    }
+}
